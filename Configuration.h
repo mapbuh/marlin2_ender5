89bc320aeb1e14f921ce07e27483e8ca1fd8b5d8
Nickola Trupcheff <n.trupcheff@gmail.com>
2020-07-23 09:46:04 +0300

    Use std values for offset

--- Configuration.h
+++ Configuration.h
@@ -964,7 +964,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 44, 14, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 44, 16, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
